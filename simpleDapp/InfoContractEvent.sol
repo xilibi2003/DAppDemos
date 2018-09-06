8a0a13c2f42e1b3f4b92a81fad2110bc02a95252
emmett <xilibi2003@gmail.com>
2018-09-06 17:54:54 +0800

    Add event

---
+++ simpleDapp/InfoContractEvent.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.4.21;
+
+contract InfoContract {
+    
+   string fName;
+   uint age;
+   
+    event Instructor(
+       string name,
+       uint age
+    );
+   
+   function setInfo(string _fName, uint _age) public {
+       fName = _fName;
+       age = _age;
+       emit Instructor(_fName, _age );
+   }
+   
+   function getInfo() public constant returns (string, uint) {
+       return (fName, age);
+   }   
+}
