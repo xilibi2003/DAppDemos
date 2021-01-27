2b07f012dae6beec9a2dcc8df07e168baf8ac272
xilibi2003@163.com <xilibi2003@163.com>
2021-01-27 10:05:29 +0800

    修复metamask 和 solidity 升级问题

--- Voting/migrations/2_migration_vote.js
+++ Voting/migrations/2_migration_vote.js
@@ -1,8 +1,5 @@
 var voting = artifacts.require("./Voting.sol");
-var safemath = artifacts.require("./SafeMath.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(safemath);
-  deployer.link(safemath, voting);
-  deployer.deploy(voting, ["Rama", "Nick", "Jose"]);
+  deployer.deploy(voting, [web3.utils.asciiToHex("Rama"), web3.utils.asciiToHex("Nick"), web3.utils.asciiToHex("Jose")]);
 };
--- Voting/truffle-config.js
+++ Voting/truffle-config.js
@@ -13,6 +13,13 @@
  */
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 7545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+    },
+
+  }
 };
